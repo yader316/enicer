6a89c848f6bf07717db08568248878f9a3729966
Yader Mendoza <yadtais316@gmail.com>
2019-12-05 21:57:54 -0600

    resolbiendo problema de menu modo movil

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,10 @@
 $(document).ready(main);
  
-var contador = 1;
- 
 function main () {
 	$('.menu__bar').click(function(){
-		if (contador == 1) {
-			$('.menu__navigation').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('.menu__navigation').animate({
-				left: '-100%'
-			});
-		}
+		// console.log('aqui');
+		$('.menu__navigation').toggleClass('active');
 	});
- 
 	// Mostramos y ocultamos submenus
 	$('.subMenu').click(function(){
 		$(this).children('.menu__children').slideToggle();
