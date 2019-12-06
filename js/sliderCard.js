8d2a80dc1d68c09f453c729aad29a107a951f76d
Yader Mendoza <yadtais316@gmail.com>
2019-12-06 12:45:52 -0600

    agregando la carpeta js

---
+++ js/sliderCard.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.history-container__card-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 9000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
