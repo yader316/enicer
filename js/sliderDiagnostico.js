b9fefa99d460a853ee4575a8fbecb8b06c31edee
Yader Mendoza <yadtais316@gmail.com>
2019-12-10 18:48:42 -0600

    se termino la pagina de servicio

---
+++ js/sliderDiagnostico.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.servicios__slider-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 9000,
+    responsive: [
+      {
+        breakpoint: 1030,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
